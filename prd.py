17eb1fe41d07be685c4fa54f22a62ae1a8179299
unknown <darrius.clk@gmail.com>
2021-07-30 13:35:22 +0800

    can look for file

--- prd.py
+++ prd.py
@@ -1,4 +1,17 @@
-product = []
+import os #operating system
+products = []
+if os.path.isfile('products.csv'):
+	print('file found') 
+	with open('products.csv','r') as f:
+		for line in f:
+			if 'product,price' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
+else:
+	print('404 not found')
+
 while True:
 	name = input('plz enter product name: ')
 	if name == "q":
@@ -7,8 +20,12 @@ while True:
 	p = []
 	p.append(name)
 	p.append(price)
-	product.append(p)
+	products.append(p)
 print(product)
 for p in products:
 	print(p[0], 'price is', p[1])
-	
+
+with open('products.csv', 'w') as f:
+	f.write('product:,price:\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
